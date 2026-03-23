d6a0aa45a4acd3e356c747e54f38e4a10dbb80cc
Umair Ashraf <ashrafumair21@gmail.com>
2026-03-24 01:39:13 +0530

    set items center and increased the logo

--- hrms-frontend/src/components/Navbar.jsx
+++ hrms-frontend/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 export default function Navbar() {
   return (
-    <div className="bg-blue-900 text-white p-4 flex justify-between">
+    <div className="bg-blue-900 text-white p-4 flex justify-between items-center ">
       
-      <h1 className="text-2xl font-bold">HRMS Lite</h1>
+      <h1 className="text-5xl font-bold">HRMS Lite</h1>
 
       <div className="space-x-4">
         <button className="px-4 py-2 bg-blue-700 rounded">
