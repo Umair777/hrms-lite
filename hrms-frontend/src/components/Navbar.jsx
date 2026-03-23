f1e4992ed77bd9b3f1ec78e4c1f48c520f8461bd
Umair Ashraf <ashrafumair21@gmail.com>
2026-03-24 01:36:50 +0530

    add the nav bar structure

--- hrms-frontend/src/components/Navbar.jsx
+++ hrms-frontend/src/components/Navbar.jsx
@@ -1,11 +1,18 @@
 export default function Navbar() {
   return (
     <div className="bg-blue-900 text-white p-4 flex justify-between">
-      <h1 className=" font-bold ">HRMS Lite</h1>
-      <div className="space-x-5">
-        <button>Employees</button>
-        <button>Attendance</button>
+      
+      <h1 className="text-2xl font-bold">HRMS Lite</h1>
+
+      <div className="space-x-4">
+        <button className="px-4 py-2 bg-blue-700 rounded">
+          Employees
+        </button>
+        <button className="px-4 py-2 bg-blue-700 rounded">
+          Attendance
+        </button>
       </div>
+
     </div>
   );
 }
