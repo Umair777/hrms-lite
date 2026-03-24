be0253c43279e9bcc0e42e8549da2c9ea94071b7
Umair Ashraf <ashrafumair21@gmail.com>
2026-03-24 10:46:40 +0530

    feat: initialize form state object

--- hrms-frontend/src/components/EmployeeForm.jsx
+++ hrms-frontend/src/components/EmployeeForm.jsx
@@ -1,12 +1,38 @@
 import { useState } from "react";
 
 export default function EmployeeForm({ onAdd }) {
+    const [form, setForm] = useState({
+    employee_id: "",
+    name: "",
+    email: "",
+    department: "",
+  });
   return (
     <form className="space-y-3">
-      <input placeholder="Employee ID" className="border p-2 w-full" />
-      <input placeholder="Full Name" className="border p-2 w-full" />
-      <input placeholder="Email" className="border p-2 w-full" />
-      <input placeholder="Department" className="border p-2 w-full" />
+      <input
+        placeholder="Employee ID"
+        className="border p-2 w-full"
+        value={form.employee_id}
+        onChange={(e) => setForm({ ...form, employee_id: e.target.value })}
+      />
+      <input
+        placeholder="Full Name"
+        className="border p-2 w-full"
+        value={form.name}
+        onChange={(e) => setForm({ ...form, name: e.target.value })}
+      />
+      <input
+        placeholder="Email"
+        className="border p-2 w-full"
+        value={form.email}
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
+      />
+      <input
+        placeholder="Department"
+        className="border p-2 w-full"
+        value={form.department}
+        onChange={(e) => setForm({ ...form, department: e.target.value })}
+      />
 
       <button className="bg-blue-500 text-white px-4 py-2">
         Add Employee
