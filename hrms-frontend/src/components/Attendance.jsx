92a84f2350bf5ef4e7919ec735710e9d638614a2
Umair Ashraf <ashrafumair21@gmail.com>
2026-03-24 22:20:15 +0530

    feature/ render attendance table structure with employee data

--- hrms-frontend/src/components/Attendance.jsx
+++ hrms-frontend/src/components/Attendance.jsx
@@ -14,8 +14,28 @@ export default function Attendance() {
 
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-4 text-blue-600">Attendance List</h2>
-      <p>Attendance data will be displayed here.</p>
+      <h2 className="text-2xl font-bold mb-4 text-blue-600">
+        Attendance List
+        </h2>
+       <table className="table-auto w-full border">
+      <thead>
+        <tr className="bg-gray-100">
+          <th className="p-2 border">Employee ID</th>
+          <th className="p-2 border">Date</th>
+          <th className="p-2 border">Status</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {attendance?.map((att) => (
+          <tr key={att.id} className="text-center border">
+            <td className="p-2 border">{att.employee_id}</td>
+            <td className="p-2 border">{att.date}</td>
+            <td className="p-2 border">{att.status}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
     </div>
   );
 }
