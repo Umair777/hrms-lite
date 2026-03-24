cbad4378c14d469d7714ceb1f45621c0b8b9f774
Umair Ashraf <ashrafumair21@gmail.com>
2026-03-24 10:41:51 +0530

    feat: add employee form UI

--- hrms-frontend/src/components/EmployeeForm.jsx
+++ hrms-frontend/src/components/EmployeeForm.jsx
@@ -2,6 +2,16 @@ import { useState } from "react";
 
 export default function EmployeeForm({ onAdd }) {
   return (
-    <h1>EmployeeForm</h1>
+    <form className="space-y-3">
+      <input placeholder="Employee ID" className="border p-2 w-full" />
+      <input placeholder="Full Name" className="border p-2 w-full" />
+      <input placeholder="Emaisl" className="border p-2 w-full" />
+      <input placeholder="Department" className="border p-2 w-full" />
+
+      <button className="bg-blue-500 text-white px-4 py-2">
+        Add Employee
+      </button>
+    </form>
+    
   );
 }
