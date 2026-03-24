d629bb566d30b86da92f913f7ba4f3d0c2d1208c
Umair Ashraf <ashrafumair21@gmail.com>
2026-03-24 13:46:54 +0530

    add conditional row delete button

--- hrms-frontend/src/components/EmployeeList.jsx
+++ hrms-frontend/src/components/EmployeeList.jsx
@@ -21,6 +21,7 @@ export default function EmployeeList({ employees, onDelete, editMode, setEditMod
                     <th className="border p-2">Name</th>
                     <th className="border p-2">Email</th>
                     <th className="border p-2">Department</th>
+                    {/* Conditional header */}
                     {editMode && (
                         <th className="border p-2">Actions</th>
                     )}
@@ -35,14 +36,21 @@ export default function EmployeeList({ employees, onDelete, editMode, setEditMod
                     <td className="border p-2">{emp.name}</td>
                     <td className="border p-2">{emp.email}</td>
                     <td className="border p-2">{emp.department}</td>
-                     <td className="border p-2">
-                        <button
-                        onClick={() => onDelete && onDelete(index)}
-                        className="bg-red-500 text-white px-2 py-1 rounded"
-                        >
-                        Delete
-                        </button>
-                    </td>
+                     {/* Conditional delete button */}
+                        {editMode && (
+                            <td className="border p-2">
+                            <button
+                                onClick={() => {
+                                
+                                    onDelete(index);
+                                
+                                }}
+                                className="bg-red-500 text-white px-2 py-1 rounded"
+                            >
+                                Delete
+                            </button>
+                            </td>
+                        )}
                     </tr>
                 ))}
                 </tbody>
