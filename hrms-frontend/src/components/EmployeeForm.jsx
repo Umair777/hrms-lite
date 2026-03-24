93ab80867592a40b32e6f6e1a836121311c6bd7c
Umair Ashraf <ashrafumair21@gmail.com>
2026-03-24 10:42:30 +0530

    formUI/fix a typo

--- hrms-frontend/src/components/EmployeeForm.jsx
+++ hrms-frontend/src/components/EmployeeForm.jsx
@@ -5,7 +5,7 @@ export default function EmployeeForm({ onAdd }) {
     <form className="space-y-3">
       <input placeholder="Employee ID" className="border p-2 w-full" />
       <input placeholder="Full Name" className="border p-2 w-full" />
-      <input placeholder="Emaisl" className="border p-2 w-full" />
+      <input placeholder="Email" className="border p-2 w-full" />
       <input placeholder="Department" className="border p-2 w-full" />
 
       <button className="bg-blue-500 text-white px-4 py-2">
